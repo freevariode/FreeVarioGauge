dba9d99e669955cf11294181f53af80ab41589c1
Blaubart <Blaubart@gmx.de>
2023-08-07 00:07:53 +0200

    including new Updates Webpages

--- src/FreeVarioGauge/FreeVarioGauge.ino
+++ src/FreeVarioGauge/FreeVarioGauge.ino
@@ -5,7 +5,7 @@
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) anyt later version.
+//    (at your option) any later version.
 //
 //    This program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -23,6 +23,10 @@
 #include <EEPROM.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+#include <display-login.html.h>
+#include <display-update.html.h>
+#include <script.js.h>
+#include <style.css.h>
 #include<SPI.h>
 #include<TFT_eSPI.h>
 #include <Preferences.h>
@@ -167,91 +171,6 @@ static unsigned long lastTimeModeWasSend = 0;
 static unsigned long lastTimeSerial2 = 0;
 unsigned long loopTime = 5000;
 
-//**********************
-//****  Login page  ****
-//**********************
-const char* loginIndex =
-  "<form name='loginForm'>"
-  "<table width='20%' bgcolor='A09F9F' align='center'>"
-  "<tr>"
-  "<td colspan=2>"
-  "<center><font size=4><b>Displayboard Login Page</b></font></center>"
-  "<br>"
-  "</td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<td>Username:</td>"
-  "<td><input type='text' size=25 name='userid'><br></td>"
-  "</tr>"
-  "<br>"
-  "<br>"
-  "<tr>"
-  "<td>Password:</td>"
-  "<td><input type='Password' size=25 name='pwd'><br></td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<tr>"
-  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-  "</tr>"
-  "</table>"
-  "</form>"
-  "<script>"
-  "function check(form)"
-  "{"
-  "if(form.userid.value=='Display' && form.pwd.value=='Display')"
-  "{"
-  "window.open('/serverIndex')"
-  "}"
-  "else"
-  "{"
-  " alert('Error Password or Username')/*displays error message*/"
-  "}"
-  "}"
-  "</script>";
-
-//*****************************
-//****  Server Index Page  ****
-//*****************************
-const char* serverIndex =
-  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-  "<center><font size=4><b>Displayboard Update Page</b></font></center>"
-  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-  "<input type='file' name='update'>"
-  "<input type='submit' value='Update'>"
-  "</form>"
-  "<div id='prg'>progress: 0%</div>"
-  "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
-  "},"
-  "error: function (a, b, c) {"
-  "}"
-  "});"
-  "});"
-  "</script>";
-
 void setup() {
   // Enable the weak pull down resistors
   ESP32Encoder::useInternalWeakPullResistors = DOWN;
@@ -387,11 +306,19 @@ void loop() {
         /*return index page which is stored in serverIndex */
         server.on("/", HTTP_GET, []() {
           server.sendHeader("Connection", "close");
-          server.send(200, "text/html", loginIndex);
+          server.send(200, "text/html", loginHTML);
         });
         server.on("/serverIndex", HTTP_GET, []() {
           server.sendHeader("Connection", "close");
-          server.send(200, "text/html", serverIndex);
+          server.send(200, "text/html", updateHTML);
+        });
+        server.on("/style.css", HTTP_GET, []() {
+          server.sendHeader("Connection", "close");
+          server.send(200, "text/css", serverCSS);
+        });
+        server.on("/script.js", HTTP_GET, []() {
+          server.sendHeader("Connection", "close");
+          server.send(200, "text/js", serverJS);
         });
         /*handling uploading firmware file */
         server.on("/update", HTTP_POST, []() {
--- src/VarioSound/VarioSound.ino
+++ src/VarioSound/VarioSound.ino
@@ -26,6 +26,10 @@
 #include <EEPROM.h>
 #include <ESPmDNS.h>
 #include <Update.h>
+#include <sound-login.html.h>
+#include <sound-update.html.h>
+#include <script.js.h>
+#include <style.css.h>
 #include <AD9833.h>                   // Include the library
 AD9833 gen(FNC_PIN);                  // Defaults to 25MHz internal reference frequency
 WebServer server(80);
@@ -86,91 +90,6 @@ long connectTime = 0;
 unsigned long pulseTime = 0;
 unsigned long startTimePulse = 0;
 unsigned long loopTime = 8000;
-
-//**********************
-//****  Login page  ****
-//**********************
-const char* loginIndex =
-  "<form name='loginForm'>"
-  "<table width='20%' bgcolor='A09F9F' align='center'>"
-  "<tr>"
-  "<td colspan=2>"
-  "<center><font size=4><b>Soundboard Login Page</b></font></center>"
-  "<br>"
-  "</td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<td>Username:</td>"
-  "<td><input type='text' size=25 name='userid'><br></td>"
-  "</tr>"
-  "<br>"
-  "<br>"
-  "<tr>"
-  "<td>Password:</td>"
-  "<td><input type='Password' size=25 name='pwd'><br></td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<tr>"
-  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-  "</tr>"
-  "</table>"
-  "</form>"
-  "<script>"
-  "function check(form)"
-  "{"
-  "if(form.userid.value=='Sound' && form.pwd.value=='Sound')"
-  "{"
-  "window.open('/serverIndex')"
-  "}"
-  "else"
-  "{"
-  " alert('Error Password or Username')/*displays error message*/"
-  "}"
-  "}"
-  "</script>";
-
-//*****************************
-//****  Server Index Page  ****
-//*****************************
-const char* serverIndex =
-  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-  "<center><font size=4><b>Soundboard Update Page</b></font></center>"
-  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-  "<input type='file' name='update'>"
-  "<input type='submit' value='Update'>"
-  "</form>"
-  "<div id='prg'>progress: 0%</div>"
-  "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
-  "},"
-  "error: function (a, b, c) {"
-  "}"
-  "});"
-  "});"
-  "</script>";
 
 /////////////////////
 // function to calculate pulse length
@@ -360,11 +279,19 @@ void loop() {
       /*return index page which is stored in serverIndex */
       server.on("/", HTTP_GET, []() {
         server.sendHeader("Connection", "close");
-        server.send(200, "text/html", loginIndex);
+        server.send(200, "text/html", loginHTML);
       });
       server.on("/serverIndex", HTTP_GET, []() {
         server.sendHeader("Connection", "close");
-        server.send(200, "text/html", serverIndex);
+        server.send(200, "text/html", updateHTML);
+      });
+      server.on("/style.css", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/css", serverCSS);
+      });
+      server.on("/script.js", HTTP_GET, []() {
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/js", serverJS);
       });
       /*handling uploading firmware file */
       server.on("/update", HTTP_POST, []() {
